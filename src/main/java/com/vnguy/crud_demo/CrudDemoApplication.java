118ad229e730645e714cc47a3b952d1cb1609362
Vinh Nguyen <vinh28062002@gmail.com>
2024-06-06 13:40:45 +0700

    start openAPI

--- src/main/java/com/vnguy/crud_demo/CrudDemoApplication.java
+++ src/main/java/com/vnguy/crud_demo/CrudDemoApplication.java
@@ -1,9 +1,11 @@
 package com.vnguy.crud_demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class CrudDemoApplication {
 
 	public static void main(String[] args) {
